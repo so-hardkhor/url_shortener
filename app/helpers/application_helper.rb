893dd013b22080f74b199ad06b7194aff56c8d08
Ann-Marie Khor <ann.mariekhor@gmail.com>
2016-08-02 15:35:20 +0800

    Install Clearance gem for user management

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	<% if signed_in? %>
+  <%= current_user.email %>
+  <%= button_to "Sign out", sign_out_path, method: :delete %>
+<% else %>
+  <%= link_to "Sign in", sign_in_path %>
+<% end %>
 end
